9527a82cb40f6e14beefcb034ab92f7863039d9d
Julian Napolitan <julian.napolitan@gmail.com>
2016-02-01 21:17:33 -0800

    Fixed errors in checkpoint 22

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -108,7 +108,7 @@ RSpec.describe AddressBook do
       book.import_from_csv("entries.csv")
       entry = book.binary_search("Bill")
       expect(entry).to be_a Entry
-      check_entry(entry, "Bill", "555-55-4854", "bill@blocmail.com")
+      check_entry(entry, "Bill", "555-555-4854", "bill@blocmail.com")
     end
 
     it "searches AddressBook for Bob" do
